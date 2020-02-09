ccde5943578232aeb802946a5c943c4d4a2b0b8f
Eli Kadosh <eli.k23@gmail.com>
2020-02-09 07:33:21 +0200

    improve code

--- Course3/my-fav-movies/src/app/components/movie-detailes/movie-detailes.component.ts
+++ Course3/my-fav-movies/src/app/components/movie-detailes/movie-detailes.component.ts
@@ -10,14 +10,21 @@ import { Subscription } from "rxjs";
 })
 export class MovieDetailesComponent implements OnInit, OnDestroy {
   movie: Movie | null = null;
+  imdb: object | null = null;
   sub: Subscription;
+  sub2: Subscription;
 
   constructor(private movieDB: MovieDBService) {}
   ngOnInit(): void {
     this.sub = this.movieDB.getMovie().subscribe(x => (this.movie = x));
+    this.sub2 = this.movieDB.getSelectedMovie().subscribe(x => {
+      this.imdb = x;
+      console.log(this.imdb);
+    });
   }
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
   // async indexChange(index: number) {
--- Course3/my-fav-movies/src/app/services/movie-db.service.ts
+++ Course3/my-fav-movies/src/app/services/movie-db.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, OnInit } from "@angular/core";
 import { Movie } from "../models/movie";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, Observer } from "rxjs";
 
 @Injectable({
   providedIn: "root"
@@ -10,18 +10,39 @@ export class MovieDBService {
   constructor(private http: HttpClient) {}
 
   movieDB: Movie[] = [];
-  private _jsonURL = "http://www.omdbapi.com/?s=iron%20man&apikey=d2286d0d";
+  apikey: string = "d2286d0d";
+  private _jsonURL =
+    "http://www.omdbapi.com/?s=iron%20man&apikey=" + this.apikey;
+  private _jsonInfoURL =
+    "http://www.omdbapi.com/?plot=full&apikey=" + this.apikey + "&i=";
 
   private selectedMovie: Movie | null = null;
   private selectedMovie$ = new BehaviorSubject(this.selectedMovie);
 
+  private selectedIMDB: object | null = null;
+  private selectedIMDB$ = new BehaviorSubject(this.selectedIMDB);
+
   selectMovie(index: number) {
     this.selectedMovie$.next(this.movieDB[index]);
+    this.getMovieFromDB(this.movieDB[index].destination);
+    //this.selectedIMDB$.next()
+  }
+
+  async getMovieFromDB(imdb: string) {
+    console.log(imdb);
+    let value = await this.getJSONImdb(imdb).subscribe(data => {
+      console.log(data);
+      this.selectedIMDB$.next(data);
+    });
   }
 
   getMovie(): Observable<Movie | null> {
     //return this.value$ as Observable<number>; // not good , user can cast
     return this.selectedMovie$.asObservable(); // much better , user cannot changen nothings
+  }
+
+  getSelectedMovie(): Observable<object | null> {
+    return this.selectedIMDB$.asObservable();
   }
 
   private async createDB() {
@@ -36,6 +57,11 @@ export class MovieDBService {
   public getJSON(): Observable<any> {
     return this.http.get(this._jsonURL);
   }
+
+  public getJSONImdb(imdb: string): Observable<any> {
+    return this.http.get(this._jsonInfoURL + imdb);
+  }
+
   private delay(): Promise<void> {
     return new Promise<void>(res => setTimeout(res, 2000));
   }
@@ -44,12 +70,5 @@ export class MovieDBService {
     this.delay();
     this.createDB();
     return this.movieDB;
-  }
-
-  async getByID(id: number): Promise<Movie> {
-    this.delay();
-    let p = this.movieDB[id];
-
-    return p;
   }
 }
